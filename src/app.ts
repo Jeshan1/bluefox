258b075fd393c28857ce29294823f595df6b2b49
Jeshan1 <jectionjection@gmail.com>
2024-12-24 23:36:24 +0545

    just for fun

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
+import { Request,Response,NextFunction } from 'express';
 import express from 'express'
-
+import { ApiError } from './helper/ApiError'
+import { globalErrorHandler } from './helper/GlobalErrorHandler';
+import * as dotenv from 'dotenv'
+import { userRoute } from './routes/userRoute';
+dotenv.config()
 const app = express()
 
 app.use(express.json())
+
+app.use('/api/',userRoute)
+
+
+app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
+    globalErrorHandler(err, res);
+  });
+
 
 
 export default app
